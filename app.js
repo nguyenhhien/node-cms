4721aa04e9e1347e3b3fe208be9832f0d4a90222
xuan_tuong <xuan_tuong@knorex.com>
2014-06-01 11:20:02 +0800

    =add cluster

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("./server/constant.js");
 
 var models              = require("./server/models");
 var express 			= require("express");
+var cluster             = require('cluster');
 var path                = require("path");
 var fs					= require("fs");
 var http				= require("http");
@@ -23,14 +24,6 @@ var restful             = require('./server/helpers/SequelizeRestfulRouter.js');
 var gzippo              = require('gzippo');
 var ActiveSuport        = require('activesupport/active-support-node.js');
 
-var SECRET    = 'wReI8rpRzLcBt7noEw7MKcR4WZhS3RL16Xyb7iH954XFLJgmiTd6u-Sqpz18wUZT';
-var AUDIENCE  = 'DyG9nCwIEofSy66QM3oo5xU6NFs3TmvT';
-
-//secret and client id for jwt tokens
-var authenticate = jwt({
-    secret: new Buffer(SECRET, 'base64'),
-    audience: AUDIENCE
-});
 
 var Database            = require("./server/database");
 
@@ -52,89 +45,104 @@ log4js.configure({
 logger = log4js.getLogger("main");
 logger.setLevel("INFO");
 
-//sync schema model definition
-Database.syncSchema();
 
-var app	= express();
+if (cluster.isMaster) {
+    //sync schema model definition
+    //TODO: add callback and only fork after sync success
+    Database.syncSchema();
 
-app.enabled('trust proxy');
-app.enable("jsonp callback");
+    var cpuCount = require('os').cpus().length;
 
-//TODO: remove; use 3 different middleware instead - express 3.0
-app.use(require("body-parser")({ uploadDir: UploadsFolder}));
+    console.log("Fork process in " + cpuCount + " CPUs");
 
-//using param validator
-app.use(expressValidator({
-    errorFormatter: function(param, msg, value) {
-        var namespace = param.split('.')
-            , root    = namespace.shift()
-            , formParam = root;
-
-        while(namespace.length) {
-            formParam += '[' + namespace.shift() + ']';
-        }
-        return {
-            param : formParam,
-            msg   : msg,
-            value : value
-        };
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
     }
-}));
-
-app.use(require("cookie-parser")());
-app.use(require("express-session")({
-    key: 'sid',
-    store:  new RedisStore({
-        host: 	Config.Redis.host,
-        port:	Config.Redis.port,
-        db:		3,
-        pass:	Config.Redis.pass,
-        client:	Database.getRedisCli()
-    }),
-    maxAge: 3600000,
-    secret: 'auth-secret'
-}));
-
-//extend response header
-require("./server/helpers/ResponseHelper.js")(http);
-
-//set necessary header -- for CORS
-app.use(function(req, res, next)
+}
+else
 {
-    res.header('Access-Control-Allow-Origin', req.get('origin')); //allow CORS
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials', true); //allow CORS
 
-    next()
-})
+    var app	= express();
 
-//setup route
-routes(app);
+    app.enabled('trust proxy');
+    app.enable("jsonp callback");
 
-//app.use("/", express.static(__dirname + "/public/build"));		// serve public files straight away
-app.use(gzippo.staticGzip(__dirname + '/public/build'));
+    //TODO: remove; use 3 different middleware instead - express 3.0
+    app.use(require("body-parser")({ uploadDir: UploadsFolder}));
 
-//setup sequelize restful API endpoint
-app.use(restful(models.sequelize, {
-    endpoint: '/api/restful',
-    allowed: [
-        {
-            tableName: "User",
-            //attributes cannot be update
-            restrictedWriteAttributes: ["email", "password"],
-            //attribute will not be returned
-            restrictedReadAttributes: ["password"],
-            //list of restful method allowed
-            allowedMethods: ["GET", "PUT"]
+    //using param validator
+    app.use(expressValidator({
+        errorFormatter: function(param, msg, value) {
+            var namespace = param.split('.')
+                , root    = namespace.shift()
+                , formParam = root;
+
+            while(namespace.length) {
+                formParam += '[' + namespace.shift() + ']';
+            }
+            return {
+                param : formParam,
+                msg   : msg,
+                value : value
+            };
         }
-    ]
-}));
+    }));
 
-app.listen(PORT, function()
-{
-    console.log("Web server process listening on %s:%d ", PORT);
-});
+    app.use(require("cookie-parser")());
+    app.use(require("express-session")({
+        key: 'sid',
+        store:  new RedisStore({
+            host: 	Config.Redis.host,
+            port:	Config.Redis.port,
+            db:		3,
+            pass:	Config.Redis.pass,
+            client:	Database.getRedisCli()
+        }),
+        maxAge: 3600000,
+        secret: 'auth-secret'
+    }));
 
+    //extend response header
+    require("./server/helpers/ResponseHelper.js")(http);
+
+    //set necessary header -- for CORS
+    app.use(function(req, res, next)
+    {
+        res.header('Access-Control-Allow-Origin', req.get('origin')); //allow CORS
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.header('Access-Control-Allow-Credentials', true); //allow CORS
+
+        next()
+    })
+
+    //setup route
+    routes(app);
+
+    //app.use("/", express.static(__dirname + "/public/build"));		// serve public files straight away
+    app.use(gzippo.staticGzip(__dirname + '/public/build'));
+
+    //setup sequelize restful API endpoint
+    app.use(restful(models.sequelize, {
+        endpoint: '/api/restful',
+        allowed: [
+            {
+                tableName: "User",
+                //attributes cannot be update
+                restrictedWriteAttributes: ["email", "password"],
+                //attribute will not be returned
+                restrictedReadAttributes: ["password"],
+                //list of restful method allowed
+                allowedMethods: ["GET", "PUT"]
+            }
+        ]
+    }));
+
+    app.listen(PORT, function()
+    {
+        console.log("Web server process listening on %s:%d ", PORT);
+    });
+}
 
 
